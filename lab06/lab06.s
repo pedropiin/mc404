5ced6b018bb20450468d62411aa2da0d2c06a78f
pedropiin <pedarosa04@gmail.com>
2024-04-16 18:06:09 -0300

    quase finalixado

--- lab06/lab06.s
+++ lab06/lab06.s
@@ -6,10 +6,13 @@ _start:
     li a7, 93 # exit
     ecall
 
-.text:
+.text
 
 main:
     # Código aqui
+    la s1, result
+    la s11, result
+
     addi sp, sp, -4
     sw ra, 0(sp)
     jal read
@@ -18,39 +21,46 @@ main:
     li s3, 4 # Limite de iterações do for, i.e., número de bytes da entrada
     li s4, 5 # Incrementador do ponteiro s0. 5 bytes para ir para o primeiro digito do proximo numero
 
-    for_main:
-        bge s2, s3, end_for_main
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal for_main
 
-        #TODO: devo somar 5 ou 40, ou seja, a soma interpreta cada unidade como byte ou bit?
-        mul t3, s2, s4
-        add s0, s0, t3
-        add s1, s1, t3
+    ret
 
-        addi sp, sp, -4
-        sw ra, 0(sp)
-        jal number_from_string
+for_main:
+    bge s2, s3, end_for_main
 
-        addi sp, sp, -4
-        sw ra, 0(sp)
-        jal my_atoi
+    #TODO: devo somar 5 ou 40, ou seja, a soma interpreta cada unidade como byte ou bit?
+    mul t3, s2, s4
+    add s0, s0, t3
+    add s1, s1, t3
 
-        addi sp, sp, -4
-        sw ra, 0(sp)
-        jal soma_digitos
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal number_from_string
 
-        addi sp, sp, -4
-        sw ra, 0(sp)
-        jal sqrt # registrador a7 guarda valor da raíz do num da iteracao
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal my_atoi
 
-        addi sp, sp, -4
-        sw ra, 0(sp)
-        jal save_answer
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal soma_digitos
 
-        addi s2, s2, 1
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal sqrt # registrador a7 guarda valor da raíz do num da iteracao
 
-        j for_main
-    end_for_main:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal save_answer
 
+    addi s2, s2, 1
+
+    j for_main
+end_for_main:
+    lw ra, 0(sp)
+    addi sp, sp, 4
     ret
 
 
@@ -135,24 +145,30 @@ sqrt:
     li a1, 0 # variável de iteração e contagem 'i'
     li a2, 10 # variável de limite do for
 
-    for_sqrt:
-        bge a1, a2, end_for_sqrt
-
-        div t1, a0, t0 # t1 = a0 / t0 ==> t1 = y / k
-        add t0, t0, t1 # t0 = t0 + t1 ==> t0 = k + t1 = k + y / k
-        div t0, t0, t2 # t0 = t0 / t2 ==> t0 = (k + y / k) / 2
-
-        addi a1, a1, 1
-
-        j for_sqrt
-    end_for_sqrt:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal for_sqrt
 
     lw ra, 0(sp)
     addi sp, sp, 4
     ret
 
-#TODO: fazer s11 apontar para o início de result
-#TODO: fazer s1 apontar para alguma posição de result
+for_sqrt:
+    bge a1, a2, end_for_sqrt
+
+    div t1, a0, t0 # t1 = a0 / t0 ==> t1 = y / k
+    add t0, t0, t1 # t0 = t0 + t1 ==> t0 = k + t1 = k + y / k
+    div t0, t0, t2 # t0 = t0 / t2 ==> t0 = (k + y / k) / 2
+
+    addi a1, a1, 1
+
+    j for_sqrt
+end_for_sqrt:
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    ret
+
+
 save_answer:
     # Função para escrever um inteiro de 4 dígitos no buffer de saída
     # no formato DDDD
@@ -178,7 +194,7 @@ save_answer:
     rem a7, a7, t0
 
     # guarda quarto bit mais significante
-    sb a7 3(s1)
+    sb a7, 3(s1)
 
     lw ra, 0(sp)
     addi sp, sp, 4
